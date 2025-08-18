98648cb938141d891ba94a0b8a355db99c3b414b
Miki Tebeka <miki@353solutions.com>
2025-08-18 04:28:06 +0000

    end of 03_06

--- Ch03/03_06/ships.py
+++ Ch03/03_06/ships.py
@@ -1,16 +1,36 @@
 # %%
+import numpy as np
+import pandera as pa
 import pandas as pd
 
 df = pd.read_csv('ships.csv')
 df
 # %%
-import pandera as pa
-import numpy as np
 
 schema = pa.DataFrameSchema({
-    'name': pa.Column(pa.String),
-    'lat': pa.Column(pa.Float),
-    'lng': pa.Column(pa.Float),
+    'name': pa.Column(
+        pa.String,
+        nullable=False,
+        checks=[
+            pa.Check.str_length(min_value=2)
+        ]
+    ),
+    'lat': pa.Column(
+        pa.Float,
+        nullable=False,
+        checks=[
+            pa.Check.ge(-90),
+            pa.Check.le(90)
+        ]
+    ),
+    'lng': pa.Column(
+        pa.Float,
+        nullable=False,
+        checks=[
+            pa.Check.ge(-180),
+            pa.Check.le(180)
+        ]
+    ),
 })
 
 schema.validate(df)
